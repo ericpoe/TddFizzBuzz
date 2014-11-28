28b2c0881261dd5d58e53ae33d2347d283b40564
Eric Poe <eric.poe@gmail.com>
2014-11-28 15:12:09 -0600

    Fix example script so it looks for 'Bang' rather than 'bang'

--- fizzbuzzbang.php
+++ fizzbuzzbang.php
@@ -8,7 +8,7 @@ $fizzer = new FizzBuzzBang();
 foreach(range(1,5000) as $num) {
     $message = $fizzer->run($num);
     echo "$message ";
-    if ('bangbangbangbang' === $message) {
+    if ('BangBangBangBang' === $message) {
         echo '!' . PHP_EOL;
         exit;
     }
