0c25a8ec0003623f3c873fc45e669d572da643f0
Eric Poe <eric.poe@gmail.com>
2014-11-30 19:00:07 -0600

    Refactor FizzBuzzBang to be more generic.

--- src/FizzBuzzBang.php
+++ src/FizzBuzzBang.php
@@ -5,8 +5,20 @@ use TddFizzBuzz\FizzBuzz;
 
 class FizzBuzzBang extends FizzBuzz
 {
+    protected $bangNum;
+    protected $out;
+
+    public function __construct($fizz = 3, $buzz = 5, $bang = 7)
+    {
+        parent::__construct($fizz, $buzz);
+        $this->bangNum = $bang;
+        $this->num = 0;
+        $this->out = '';
+    }
+
     public function run($num)
     {
+        $this->setNum($num);
         $this->out = parent::run($num);
 
         if (!$this->isFizz() && !$this->isBuzz()) {
@@ -22,6 +34,15 @@ class FizzBuzzBang extends FizzBuzz
         return $this->out;
     }
 
+    protected function bangOut()
+    {
+        $temp = $this->num;
+        while($this->isBang($temp)) {
+            $this->out .= 'Bang';
+            $temp /= $this->bangNum;
+        }
+    }
+
     protected function isBang($num = null)
     {
         // If $num is not explicitly provided, grab the implicit $num
@@ -29,15 +50,6 @@ class FizzBuzzBang extends FizzBuzz
             $num = $this->num;
         }
 
-        return 0 === $num % 7;
-    }
-
-    protected function bangOut()
-    {
-        $temp = $this->num;
-        while($this->isBang($temp)) {
-            $this->out .= 'Bang';
-            $temp /= 7;
-        }
+        return 0 === $num % $this->bangNum;
     }
 }
